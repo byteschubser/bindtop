f7b6234a70813fe96db2d05336c9855ab24e21dd
Uwe Kiewel <uwe@kiewel-online.ch>
2013-02-23 15:52:17 +0100

    added: - short hint if there are wrong command line parameters - delete temp_file forced at program start - startet tu write README and NEWS

--- src/main.c
+++ src/main.c
@@ -13,7 +13,13 @@
 int main(int argc, char *argv[]) {
 	bindtop_Settings_Type bindtop_Settings;
 
-	bindtop_Settings = readConfigFile(argv[1]);
-	getBindStatisticsZabbix(bindtop_Settings);
-
+	if ( argc != 2) {
+		fprintf(stderr, "%s <config file>\n", argv[0]);
+		return EXIT_FAILURE;
+	} else {
+		bindtop_Settings = readConfigFile(argv[1]);
+		delXMLFile(bindtop_Settings.tmp_file);
+		getBindStatisticsZabbix(bindtop_Settings);
+		return EXIT_SUCCESS;
+	}
 }
